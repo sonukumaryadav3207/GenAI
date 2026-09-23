c7242f88018a20af1317326b46fa65d687046a97
sonukumaryadav3207 <sonukumaryadav3207@gmail.com>
2026-09-23 19:30:49 +0530

    streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,33 @@
 import streamlit as st
+import pandas as pd
 
-st.title("My First Streamlit App")
+st.write("Hello world")
 
-st.write("Hello GenAI!")
+st.title("Hello streamlit")
+st.write("This is my first streamlit App")
+
+st.header("Welcome to streamlit")
+st.subheader("This is subheader")
+st.text("This is plain text")
+
+
+## buttons , checkbox , and sliders
+
+if st.button("Click me"):
+    st.write("Button clicked")
+
+agre=st.checkbox("I agree")
+if  agre:
+    st.write("You agree")
+
+level =st.slider("select a level : ", 1,10,5)
+st.write(f"selected level : {level}")
+
+
+uploaded_file= st.file_uploader("Upload a file",type=["csv","txt"])
+
+if uploaded_file is not None :
+    df=pd.read_csv(uploaded_file)
+    st.write(df.head())
+    
+
